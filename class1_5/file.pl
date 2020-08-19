9ccee7dfd91392b67f81f27b3e5ca8e9ba98f7b0
bruno_diego5 <bruno_diego5@hotmail.com>
2020-08-18 23:04:40 -0300

    class 1.5

---
+++ class1_5/file.pl
@@ -0,0 +1,23 @@
+#scalar variable and typing
+
+$var = "Hello";
+print "$var\n";
+
+$var = 7;
+print "$var\n";
+
+$var = 8 + 7.5;
+print "$var\n";
+
+chomp($v1 = <STDIN>);
+chomp($v2 = <STDIN>);
+
+print $v1 + $v2;print "\n";
+print $v1 . $v2;print "\n";
+
+#Scalars variable
+$var = 7.5, "variable";
+print "$var\n";
+
+$var = (7.5, "variable", 90);
+print "$var\n";
