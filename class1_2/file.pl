8b315ed06985b5e7ccc99d61347ed0b19fa94050
bruno_diego5 <bruno_diego5@hotmail.com>
2020-08-18 22:35:14 -0300

    class 1.2

---
+++ class1_2/file.pl
@@ -0,0 +1,12 @@
+#escape characters
+
+#\n = new line
+#\t = tab
+#\\ = print \
+#\" = print "
+#\' = print '
+
+print "\"Hello World\" \n\n"; print(7+3);
+
+#escape characters like literal
+print '\n "double quotes" \'single quotes\' ';
