052dce659f2e0843fd227668f0db07d3f09a05dd
bruno_diego5 <bruno_diego5@hotmail.com>
2020-08-28 23:57:50 -0300

    class 03.01 - list

---
+++ class_03_01/file.pl
@@ -0,0 +1,15 @@
+#list
+
+$var = "variable";
+($v1, $v2, $v3, $v4, $v5)=(1, 55.7, $var, "Hello", -10);
+print "v1 : $v1 \n";
+print "v2 : $v2 \n";
+print "v3 : $v3 \n";
+print "v4 : $v4 \n";
+print "v5 : $v5 \n";
+print "\n";
+
+#$_ is read-only to fixed value
+foreach (1, 55.7, $var, "Hello", -10) {
+    print "element: $_\n";
+}
